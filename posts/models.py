40012a71c82ed3d04f9e5c5c8b0e227a9303c06e
Ardiantohendy <bayuardianto421@gmail.com>
2025-02-10 16:46:46 +0700

    new commit

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    image = models.CharField(max_length=100)
+    content = models.CharField(max_length=10000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} - {self.created_at}'
