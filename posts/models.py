4ee3ef93347bb15719d16b9cc5fb276420720516
Ardiantohendy <bayuardianto421@gmail.com>
2025-02-11 11:10:00 +0700

    new commit

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=300)
-    image = models.CharField(max_length=100)
-    content = models.CharField(max_length=10000)
+    content = models.TextField()
+    image = models.ImageField(upload_to='artikel/', blank=True, null=True)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
 
     def __str__(self):
-        return f'{self.title} - {self.created_at}'
+        return self.title
