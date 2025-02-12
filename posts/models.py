56fd89bf6dca1e21df443dde44dcd861d6071562
Ardiantohendy <bayuardianto421@gmail.com>
2025-02-12 10:51:38 +0700

    new commit

--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=300)
     content = models.TextField()
-    image = models.ImageField(upload_to='artikel/', blank=True, null=True)
+    image = models.ImageField(upload_to='article/', blank=True, null=True)
     writer = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
