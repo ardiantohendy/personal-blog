c2ccad4026c5809ade7eb85fe21128ad17f9f2a3
Ardiantohendy <bayuardianto421@gmail.com>
2025-02-13 11:06:52 +0700

    adding register in views.py

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 
 # Create your views here.
@@ -7,3 +9,32 @@ from .models import Post
 def index(request):
     post_list = Post.objects.all()
     return render(request, 'index.html', {'post_list': post_list})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exist')
+                return redirect('register')
+            
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already used')
+                return redirect('register')
+
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+
+        else:
+            messages.info(request, "Password not the same")
+            return redirect('register')
+
+    else:
+        return render(request, 'register.html')
