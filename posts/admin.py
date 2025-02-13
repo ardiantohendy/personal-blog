58f4a2259a83eb46d46bbc8274523f82e9ec7de3
Ardiantohendy <bayuardianto421@gmail.com>
2025-02-13 15:49:01 +0700

    adding like and bookmark in models and migrate it all

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Like, Bookmark
 
 # Register your models here.
 
+admin.site.register(Bookmark)
+admin.site.register(Like)
 admin.site.register(Post)
--- posts/models.py
+++ posts/models.py
@@ -16,3 +16,29 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'post'], name='unique_user_post_like')
+        ]
+    
+    def __str__(self):
+        return f'{self.user.username} likes {self.post.title}'
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='bookmarks', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'post'], name='unique_user_post_bookmark')
+        ]
+
+    def __str__(self):
+        return f'{self.user.username} bookmarked {self.post.title}'
