e6e725ab2ba569c8f1646a2db0147091b52b44e7
Ardiantohendy <bayuardianto421@gmail.com>
2025-02-13 12:37:26 +0700

    new commit -> adding register form

--- posts/urls.py
+++ posts/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('register', views.register, name = 'register'),
 ]
 
 if settings.DEBUG:
